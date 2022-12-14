bbfa17de4ecec27d21060365b00c35f5a4d1bc36
hyewon1007 <71242452+hyewon1007@users.noreply.github.com>
2022-12-14 22:43:22 +0900

    hw 그래프 스타일 완성

--- src/pages/Page14.js
+++ src/pages/Page14.js
@@ -2,7 +2,7 @@ import page_13_sub_image from "../image/sub-image/page-13/page-13-sub-image.png"
 import page_13_sub_image_4 from "../image/sub-image/page-13/page-13-sub-image-4.png";
 import page_13_sub_image_2 from "../image/sub-image/page-13/page-13-sub-image-2.png";
 import page_13_sub_image_3 from "../image/sub-image/page-13/page-13-sub-image-3.png";
-
+<img className="w250px" src={page_13_sub_image_2} alt="page_13_sub_image_2" />;
 
 export default function Page() {
   return (
@@ -29,28 +29,51 @@ export default function Page() {
 
             <div className="w-100 flex h1207">
               <div className="w-50 h-100 flex jcs left-container mt50">
-                <div className="flc fc">
-                  <div className="page-13-sub-image-1">
-                    <img
-                      style={{ width: 370 }}
-                      src={page_13_sub_image}
-                      alt="page_13_sub_image"
-                    />
+                <div class="leadership-type flc fc mt35">
+                  <div class="page-13-sub-image-1 flex">
+                    <div class="type-left">
+                      <div class="type-h flc jcs fwb">H</div>
+                      <hr />
+                      <div
+                        class="wmvkr mt15 mb15 fwb"
+                        style={{ letterSpacing: "-1.5px" }}
+                      >
+                        지지행동
+                      </div>
+                      <hr />
+                    </div>
+                    <div class="flc fc type-block-container">
+                      <div class="flc">
+                        <div class="block active">지원형</div>
+                        <div class="block">코칭형</div>
+                      </div>
+                      <div class="flc">
+                        <div class="block">위임형</div>
+                        <div class="block">지시형</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="type-right">
+                    <div class="type-l flc jcs fwb">L</div>
+                    <hr />
+                    <div class="ml15 mr15 fwb">지시행동</div>
+                    <hr />
+                    <div class="type-bh flc jcs fwb">H</div>
                   </div>
 
-                  <div className="p0-27 mt50">
-                    <div className="pr-title">나의 리더십 유형</div>
+                  <div class="p0-27 mt50">
+                    <div class="pr-title">나의 리더십 유형</div>
 
-                    <div className="text-container">
-                      <div className="mb30 mt30">
-                        <div className="flc jcs">
+                    <div class="text-container">
+                      <div class="mb30 mt30">
+                        <div class="flc jcs">
                           <img
-                            className="w250px"
+                            style={{ width: 250 }}
                             src={page_13_sub_image_2}
                             alt="page_13_sub_image_2"
                           />
                         </div>
-                        <div className="mb10">특징</div>
+                        <div class="main-sub-title mb10">특징</div>
                         <ul>
                           <li>
                             구성원들의 정서적인 측면(지지행동)과 업무 목표
@@ -59,8 +82,8 @@ export default function Page() {
                         </ul>
                       </div>
 
-                      <div className="mb30">
-                        <div className="mb10">강점</div>
+                      <div class="mb30">
+                        <div class="main-sub-title mb10">강점</div>
                         <ul>
                           <li>
                             직원을 인격적으로 존중하고, 긴밀한 유대관계
@@ -75,8 +98,8 @@ export default function Page() {
                         </ul>
                       </div>
 
-                      <div className="mb30">
-                        <div className="mb10">주의할 점</div>
+                      <div class="mb30">
+                        <div class="main-sub-title mb10">주의할 점</div>
                         <ul>
                           <li>
                             직원들의 직무 발단 단계에 따른 지시행동&지지행동의
@@ -92,11 +115,54 @@ export default function Page() {
               <div className="w-50 h-100 flex jcs mt50">
                 <div className="flc fc">
                   <div className="page-13-sub-image-1">
-                    <img
-                      style={{ width: 370 }}
-                      src={page_13_sub_image_4}
-                      alt="page_13_sub_image_4"
-                    />
+                    <div class="leadership-style-label flc">
+                      <p></p>
+                      <div class="fwb f14 ml5">리더십 스타일</div>
+                    </div>
+                    <div class="leadership-style flc jcs fc">
+                      <div class="fwb f18 mb15">유연성/자율성</div>
+                      <div class="flc">
+                        <div
+                          class="fwb f18 wmvkr mr15"
+                          style={{ letterSpacing: "-1.5px" }}
+                        >
+                          내부지향성
+                        </div>
+                        <div class="flc fc">
+                          <div class="flc">
+                            <div class="fwb block mr5 mb5">
+                              공동체형
+                              <div class="triangle section1">촉진자</div>
+                              <div class="triangle section2">조언자</div>
+                            </div>
+                            <div class="flex jce fwb block mb5">
+                              혁신중시형
+                              <div class="triangle section3">개혁자</div>
+                              <div class="triangle section4">중재자</div>
+                            </div>
+                          </div>
+                          <div class="flc">
+                            <div class="flex afe fwb block mr5 mb5">
+                              위계질서형
+                              <div class="triangle section5">감시자</div>
+                              <div class="triangle section6 active">조정자</div>
+                            </div>
+                            <div class="flex jce afe fwb block mb5">
+                              성과중시형
+                              <div class="triangle section7">감독자</div>
+                              <div class="triangle section8">성취자</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div
+                          class="fwb f18 wmvkr ml15"
+                          style={{ letterSpacing: "-1.5px" }}
+                        >
+                          외부지향성
+                        </div>
+                      </div>
+                      <div class="fwb f18 mt15">안정성</div>
+                    </div>
                   </div>
 
                   <div className="p0-27 mt50">
