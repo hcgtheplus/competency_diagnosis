760d5bbf83a7a65150135436268f1dcdb85689d5
WSKimHCG <93896551+WSKimHCG@users.noreply.github.com>
2022-12-15 20:19:33 +0900

    20 페이지 작업중(40%), 22페이지 console.log 삭제

--- src/pages/Page20.js
+++ src/pages/Page20.js
@@ -1,4 +1,33 @@
-export default function Page() {
+export default function Page({ data }) {
+  const text123 = data
+    .filter(
+      (d) =>
+        d?.C?.includes("Text 1") ||
+        d?.C?.includes("Text 2") ||
+        d?.C?.includes("Text 3")
+    )
+    .map((d) => d.Column6 && String(d.Column6).trim());
+
+  const chart1Label = data[4];
+  const LeadingBusiness = data[5];
+  const 비전제시 = data[6];
+  const 합리적의사결정 = data[7];
+  const 강력한실행력 = data[8];
+  const chart2column = data[10];
+  const chart2total = data[11];
+  const chart2비전제시 = data[12];
+  const chart2비즈니스감각 = data[13];
+  const chart2혁신적사고 = data[14];
+  const chart2변화주도 = data[15];
+  const chart2합리적의사결정 = data[16];
+  const chart2고객중심 = data[17];
+  const chart2전문성 = data[18];
+  const chart2전략적판단 = data[19];
+  const chart2강력한실행력 = data[20];
+  const chart2전략적네트워크 = data[21];
+  const chart2위험관리 = data[22];
+  const chart2추진력 = data[23];
+
   return (
     <div class="image-container page 20 pr">
       <div class="page-absolute">20 / 36</div>
@@ -18,21 +47,22 @@ export default function Page() {
               <div class="text-container mt50">
                 <h5 class="mb10">총평</h5>
                 <ul>
-                  <li>
-                    홍길동 님은 본인 Leading Business 역할에 대하여 우수한
-                    수준이라고 평가하셨으며, 이는 타인 진단 결과 대비 유사한
-                    수준입니다.
-                  </li>
+                  {text123[0] ? <li>{text123[0]}</li> : ""}
+                  {text123[1] ? <li>{text123[1]}</li> : ""}
                   <li>
                     홍길동 님의 Leading Business 역할은 4.1으로 이는
                     회사內유사한 수준이며, 소속 조직內유사한 수준입니다.
                   </li>
                 </ul>
                 <div class="info mt20">
-                  <span class="material-symbols-outlined">arrow_right</span>
-                  회사 평균 대비 +0.5 초과인 경우 “우수한” 수준, -0.5~0.5 사이인
-                  경우 “양호한“ 수준, -0.5 미만인 경우 “낮은“ 수준으로
-                  표현하였습니다.
+                  {text123[2] ? (
+                    <>
+                      <span class="material-symbols-outlined">arrow_right</span>
+                      {text123[2]}
+                    </>
+                  ) : (
+                    ""
+                  )}
                 </div>
               </div>
               <div class="text-container mt50">
@@ -42,71 +72,169 @@ export default function Page() {
                 <div>
                   <div class="flc mr20 mb10">
                     <div class="page-20-legend"></div>
-                    <div class="f16">우리 부서</div>
+                    <div class="f16">{chart1Label.E}</div>
                   </div>
                   <div class="flc mb10">
                     <div class="page-20-legend sec"></div>
-                    <div class="f16">타인진단(상향+하향)</div>
+                    <div class="f16">{chart1Label.F}</div>
                   </div>
                   <div class="flc mb10">
                     <div class="page-20-legend thr"></div>
-                    <div class="f16">상향진단</div>
+                    <div class="f16">{chart1Label.Column9}</div>
                   </div>
                   <div class="flc mb10">
                     <div class="page-20-legend four"></div>
-                    <div class="f16">우리 회사</div>
+                    <div class="f16">{chart1Label.H}</div>
                   </div>
                 </div>
 
                 <div class="page-18-grape-img-data">
                   <div class="page-20-grape-item pr">
                     <div class="flex jcs h125 al-end">
-                      <div class="page-20-our-department mr10">3.8</div>
-                      <div class="page-20-our-department sec mr10">3.7</div>
-                      <div class="page-20-our-department company thr mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department mr10"
+                        style={{
+                          height: `${LeadingBusiness.E.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {LeadingBusiness.E.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company four">3.7</div>
+                      <div
+                        class="page-20-our-department sec mr10"
+                        style={{
+                          height: `${LeadingBusiness.F.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {LeadingBusiness.F.toFixed(1)}
+                      </div>
+                      <div
+                        class="page-20-our-department company thr mr10"
+                        style={{
+                          height: `${LeadingBusiness.Column9.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {LeadingBusiness.Column9.toFixed(1)}
+                      </div>
+                      <div
+                        class="page-20-our-department company four"
+                        style={{
+                          height: `${LeadingBusiness.H.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {LeadingBusiness.H.toFixed(1)}
+                      </div>
                     </div>
                     <div class="page-20-active-item"></div>
                     <div class="top-line-page-20"></div>
                   </div>
                   <div class="page-20-grape-item">
                     <div class="flex jcs h125 al-end">
-                      <div class="page-20-our-department mr10">3.8</div>
-                      <div class="page-20-our-department company sec mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department mr10"
+                        style={{
+                          height: `${비전제시.E.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {비전제시.E.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company thr mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department company sec mr10"
+                        style={{
+                          height: `${비전제시.F.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {비전제시.F.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company four">3.7</div>
+                      <div
+                        class="page-20-our-department company thr mr10"
+                        style={{
+                          height: `${비전제시.Column9.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {비전제시.Column9.toFixed(1)}
+                      </div>
+                      <div
+                        class="page-20-our-department company four"
+                        style={{
+                          height: `${비전제시.H.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {비전제시.H.toFixed(1)}
+                      </div>
                     </div>
                     <div class="top-line-page-20"></div>
                   </div>
                   <div class="page-20-grape-item">
                     <div class="flex jcs h125 al-end">
-                      <div class="page-20-our-department mr10">3.8</div>
-                      <div class="page-20-our-department company sec mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department mr10"
+                        style={{
+                          height: `${합리적의사결정.E.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {합리적의사결정.E.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company thr mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department company sec mr10"
+                        style={{
+                          height: `${합리적의사결정.F.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {합리적의사결정.F.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company four">3.7</div>
+                      <div
+                        class="page-20-our-department company thr mr10"
+                        style={{
+                          height: `${합리적의사결정.Column9.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {합리적의사결정.Column9.toFixed(1)}
+                      </div>
+                      <div
+                        class="page-20-our-department company four"
+                        style={{
+                          height: `${합리적의사결정.H.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {합리적의사결정.H.toFixed(1)}
+                      </div>
                     </div>
                     <div class="top-line-page-20"></div>
                   </div>
                   <div class="page-20-grape-item">
                     <div class="flex jcs h125 al-end">
-                      <div class="page-20-our-department mr10">3.8</div>
-                      <div class="page-20-our-department company sec mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department mr10"
+                        style={{
+                          height: `${강력한실행력.E.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {강력한실행력.E.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company thr mr10">
-                        3.7
+                      <div
+                        class="page-20-our-department company sec mr10"
+                        style={{
+                          height: `${강력한실행력.F.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {강력한실행력.F.toFixed(1)}
                       </div>
-                      <div class="page-20-our-department company four">3.7</div>
+                      <div
+                        class="page-20-our-department company thr mr10"
+                        style={{
+                          height: `${강력한실행력.Column9.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {강력한실행력.Column9.toFixed(1)}
+                      </div>
+                      <div
+                        class="page-20-our-department company four"
+                        style={{
+                          height: `${강력한실행력.H.toFixed(1) * 20}%`,
+                        }}
+                      >
+                        {강력한실행력.H.toFixed(1)}
+                      </div>
                     </div>
                     <div class="top-line-page-20"></div>
                   </div>
--- src/pages/Page22.js
+++ src/pages/Page22.js
@@ -1,5 +1,4 @@
 export default function Page({ data }) {
-  console.log(data);
   const text123 = data
     .filter(
       (d) =>
