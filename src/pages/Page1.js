22aa6d7e6d74b83e5d5e1caeb9f4443c01ee1854
kang jin soo-001 <wlstnrkd124@gmail.com>
2022-12-15 13:51:29 +0900

    page-1 유저네임, 부서 퍼블리싱

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -3,6 +3,10 @@ import page_1 from "../image/page-1.png";
 export default function Page() {
   return (
     <div className="pr" style={{ pageBreakBefore: "avoid" }}>
+      <div className="page-1-user-name">
+        <div className="page-1-department">미래전략기획부</div>
+        <div className="page-1-user-name">홍길동</div>
+      </div>
       <img className="page 1" src={page_1} alt="page_1" />
     </div>
   );
